662eec5fdf181c93234f0baba355d5379b9fc81d
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-10-02 10:43:11 +0530

    Create Exp17A.cpp

---
+++ Exp17A.cpp
@@ -0,0 +1,24 @@
+// EXPERIMENT 17A : Creating Nodes and Linking them
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    cout << (*head).val << " " << (*(*head).next).val << endl;
+    return 0;
+}
